40f7ce06aa53497281725b2d3ed5d3623a972222
archiepvc <zoecolliedog@gmail.com>
2024-09-10 22:16:30 +1200

    diff PC

--- music_starter.js
+++ music_starter.js
@@ -1,42 +1,96 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(0)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
-   console.log("counter: " + counter);
+console.log("counter: " + counter);
+
+let firstRun = true
+
+let Snail = [];{
+  if (firstRun) {
+    rectMode(CENTER);
+    Snail.push(loadImage('happySnail_0.png'));
+    Snail.push(loadImage('happySnail_1.png'));
+    Snail.push(loadImage('happySnail_2.png'));
+    firstRun = false
+  }
+
+  background(0);
+
+  let VocalFrame = int(map(vocal, 0,100, 0,3));
+
+  console.log(VocalFrame);
+  push();
+  scale(0.5);
+  image(Snail[VocalFrame], width/2, height/2)
+  pop();
 
 
-   if(counter>= 100 && counter <= 300){
-   fill(100)
-   textAlign(CENTER)
-   textSize(100)
-   text("BEIRD", 600, 300); // TITLE !
 }
-let TV = map(drum, 0, 190, 500, 600)
-let Speaker = map(vocal, 0, 100, 500, 600)
 
+let TV = map(drum, 0, 200, 500, 600);
+let Speaker = map(vocal, 0, 150, 500, 600);
+
+// This Draws the TV Set.
 fill(117, 76, 41)
-rect(580, 440, TV + 420, TV + 90)
+rect(0, 970, 4000, 250);
 
-fill(209, 178, 140)
-rect(530,440, TV + 260, TV + 40)
+fill(82, 50, 23);
+rect(900, 750, 75, 250);
+rect(250, 750, 75, 250);
 
-scale(0.87)
-stroke(1)
-strokeWeight(2)
-fill(80, 119, 148)
-rect(605,500, TV + 200, TV)
+fill(117, 76, 41);
+rect(580, 440, TV + 420, TV + 90);
 
-//stroke(0)
-//strokeWeight(0)
-//fill(166, 116, 73)
-//rect(690, 875, Speaker+350, Speaker-620)
+fill(173, 143, 106);
+rect(530,440, TV + 260, TV + 40);
+
+fill(56, 38, 16);
+rect(985 + TV/5 - 100,440 + TV/5 - 100, Speaker - 470, Speaker); //- TV + 500) - TV + 40
+
+if(counter>= 1285 && counter <= 7000){
+   scale(0.87);
+   stroke(1);
+   strokeWeight(2);
+   fill(0);
+   rect(605,500, TV + 200, TV);   
+}
+else
+{if(counter<= 1285)
+   scale(0.87);
+   stroke(1);
+   strokeWeight(2);
+   fill(56, 88, 112);
+   rect(605,500, TV + 200, TV);
+}
+
+if(counter>= 0 && counter <= 300){
+   textAlign(CENTER)
+}
+
+//this displays the "words"
+if(counter>= 0 && counter <= 370){
+fill(167, 195, 217);
+textSize(100);
+textFont('Courier');
+text("BEIRD", 562 + TV/10, 465 + TV/10);
+}
+
+if(counter>= 692 && counter <= 771){
+   fill(167, 195, 217);
+   textSize(100);
+   textFont('Courier');
+   text("BY", 562 + TV/10, 465 + TV/10);
+}
+if(counter>= 848 && counter <= 1002){
+   fill(167, 195, 217);
+   textSize(70);
+   textFont('Courier');
+   text("JACK STAUBER", 562 + TV/10, 465 + TV/10);
+}
+
+
+}
+
 
 
 
@@ -47,60 +101,9 @@ rect(605,500, TV + 200, TV)
 //var Linestart = 100;
 //var LineEnd = Linestart*LengthOfLine;
 
-
 //for(var i = 1; i <=5; i ++){
 // var loopY = i*70;
 //strokeWeight(10)
 // line (Linestart, drumMap*i, LineEnd, drumMap*i);
 //}
-
-
-
-//fill(500)
-//ellipse(200, 200, eyeSize, eyeSize)
-//ellipse(400, 200, eyeSize, eyeSize)
-
-
-//fill(50, 100, 100)
-//ellipse(200, 200, eyeSize/2, eyeSize/2)
-//ellipse(400, 200, eyeSize/2, eyeSize/2)
-
-//let mouthSize = map(vocal, 0, 100, 2, 50)
-//fill(500)
-//rect(300, 400, 300, mouthSize)
-
-textFont('Verdana'); // please use CSS safe fonts
-rectMode(CENTER)
-textSize(24);
-}
-
-//vocal bar is red
-//fill(200, 0, 0);
-//rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-//fill(0);
-//text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
-   // drum bar is green
- //fill(0, 200, 0);
-// rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-//fill(0);
-//text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-//fill(50, 50, 240);
-//rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-//fill(0);
-//text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-//fill(200, 200, 200);
-//rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-//fill(0);
-//text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-//fill(255, 255, 0);
- 
-   // display "words"
-//textAlign(CENTER);
-//textSize(vocal);
-//text(words, width/2, height/3);
-//}
